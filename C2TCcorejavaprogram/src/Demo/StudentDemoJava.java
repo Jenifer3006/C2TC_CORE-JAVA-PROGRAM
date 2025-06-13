0b43a9fb84a0e667f2d77cf7d3383f236f4198de
jg652 <jg652@jenifer>
2025-06-13 19:02:27 +0530

    java practical programs

---
+++ C2TCcorejavaprogram/src/Demo/StudentDemoJava.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class StudentDemoJava {
+      public static void main(string args[]) {
+	   Student st = new Student();
+	    st.setId(101);
+	    st.setName("John");
+	    st.setAge("20");
+	    
+System.our.println("Id:"+st.getId());
+System.out.println("Name:"st.getName());
+
+
+		
+	
+	
+	}
+
--- C2TCcorejavaprogram/src/day1/DataTypesDemo.java
+++ C2TCcorejavaprogram/src/day1/DataTypesDemo.java
@@ -2,8 +2,19 @@ package day1;
 
 public class DataTypesDemo {
 
-	public DataTypesDemo() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+		
+		int value = 9/2 ;
+		float value2 = 101/61;
+		double value3= 10d/ 6d;
+		float value4= 5/2.5f;
+		System.out.println("value 1=" + value);
+		System.out.println("value 2 =" + value);
+		System.out.println("value 3=" + value );
+		System.out.println("value 4= "+ value);
+		
+			int marker = 512;
+			double percentage = marker*0.46f;
+			System.out.println("percentage :"+ percentage);
 	}
-
 }
