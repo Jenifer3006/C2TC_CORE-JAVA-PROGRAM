da466975c06aa9d60c2dca6b9bc281c638ffb910
jg652 <jg652@jenifer.mshome.net>
2025-06-19 12:12:54 +0530

    program for single inheritance

---
+++ C2TCcorejavaprogram/src/day5/stinheritdemo.java
@@ -0,0 +1,8 @@
+package day5;
+
+public class stinheritdemo{
+	public static void main(String args[]) {
+		Trainer t1 =new Trainer(12,"jenifer","chennai","student","agni college");
+		System.out.println(t1);
+	}
+}
